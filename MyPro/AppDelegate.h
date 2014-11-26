d540e59542f5f39b5626f46ea96b0c597dc97272
sk <sk@sktekiMacBook-Pro.local>
2014-11-26 14:17:07 +0800

    Initial Commit

---
+++ MyPro/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyPro
+//
+//  Created by sk on 14/11/26.
+//  Copyright (c) 2014年 sk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyPro/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyPro
+//
+//  Created by sk on 14/11/26.
+//  Copyright (c) 2014年 sk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
